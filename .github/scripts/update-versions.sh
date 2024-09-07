7ba3343615100870b03313ff907c926908732389
Pau RE <prodrigestivill@gmail.com>
2024-09-07 18:48:41 +0200

    Add initial github actions to update version

---
+++ .github/scripts/update-versions.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -ae
+
+cd "$(dirname "$0")/../.."
+
+getLatestReleaseTag() {
+  echo "Checking latest release version for $1..." > /dev/stderr
+  gh release view --repo "$1" --json tagName --template '{{.tagName}}'
+}
+
+R2AI_VERSION=$(getLatestReleaseTag radareorg/r2ai)
+
+echo "Updating versions in snap/snapcraft.yaml..." > /dev/stderr
+yq eval -i '.version=strenv(R2AI_VERSION)' snap/snapcraft.yaml
